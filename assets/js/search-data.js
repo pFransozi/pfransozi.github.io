18e4e330eb5b6d85cefbc41bb3a5f8acdf5a788b
pFransozi <90290165+pFransozi@users.noreply.github.com>
2025-11-30 20:54:20 +0000

    Deploying to gh-pages from @ pFransozi/pfransozi.github.io@730281f6f0dc4d9f6996f2c6528f82242be7c5fc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,18 +440,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-seleção-de-características-multiobjetivo-para-detecção-de-malwares-android",
+            },},{id: "projects-seleção-de-características-multiobjetivo-para-detecção-de-malwares-android",
           title: 'Seleção de Características Multiobjetivo para Detecção de Malwares Android',
           description: "NSGA-II aplicado a múltiplas visões estáticas de APKs",
           section: "Projects",handler: () => {
@@ -468,35 +457,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%70%68%69%6C%69%70%65%66%72%61%6E%73%6F%7A%69@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/pfransozi", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/philipehf", "_blank");
         },
       },{
       id: 'light-theme',
