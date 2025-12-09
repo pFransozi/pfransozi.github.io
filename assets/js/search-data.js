fdf9bcf6d305754ab4fb78116c4f4fe46d2c8970
pFransozi <90290165+pFransozi@users.noreply.github.com>
2025-12-09 03:48:50 +0000

    Deploying to gh-pages from @ pFransozi/pfransozi.github.io@2b2bd2940967c167bc5d84d77ed1f897946b3410 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -65,6 +65,11 @@ ninja.data = [{
           description: "Modelo leve de detecção de malwares Android com seleção de características multiobjetivo.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/lightweight-android-malware-detection/";
+            },},{id: "teaching-snake-com-p5-js-construindo-o-clássico-jogo-da-cobrinha",
+          title: 'Snake com P5.js: construindo o clássico jogo da cobrinha',
+          description: "Projeto guiado de Pensamento Computacional para 1º ano, programando o jogo Snake em JavaScript com a biblioteca P5.js.",
+          section: "Teaching",handler: () => {
+              window.location.href = "/teaching/teaching-snake-p5js/";
             },},{id: "teaching-funções-em-javascript-desenvolvendo-um-recomendador-de-filmes",
           title: 'Funções em JavaScript: desenvolvendo um recomendador de filmes',
           description: "Projeto guiado de Pensamento Computacional para 1º ano, usando funções, condicionais e lógica de recomendação em JavaScript.",
